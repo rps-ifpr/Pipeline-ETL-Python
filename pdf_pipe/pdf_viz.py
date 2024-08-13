56ecf8f7b095f678d98fc7a65f5d7d22c5e77572
Rogério Santos <rogerio.dosantos@ifpr.edu.br>
2024-08-13 20:52:06 -0300

    UpDate

---
+++ pdf_pipe/pdf_viz.py
@@ -0,0 +1,14 @@
+import os
+import camelot
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
+file_name = "fatura1"
+path = os.
+
+table = camelot.read_pdf(
+path,
+pages=
+flavor=
+)
